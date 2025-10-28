6b3e27852489e3a040a6db97ca9cf4842d2b12be
koo2957-collab <koo2957@gmail.com>
2025-10-28 19:32:35 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ function getFirebaseConfig() {
         authDomain: "project-3907456119741146864.firebaseapp.com",
         databaseURL: "https://project-3907456119741146864-default-rtdb.asia-southeast1.firebasedatabase.app",
         projectId: "project-3907456119741146864",
-        storageBucket: "project-3907456119741146864.firebasestorage.app",
+        storageBucket: "project-3907456119741146864.appspot.com",
         messagingSenderId: "690596721309",
         appId: "1:690596721309:web:0fc22a9d137529c3ed9a74"
     };
